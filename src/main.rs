f5d36990c0b7bb649282a379e4f14bdfecc53386
Rajshri Patil <rajshripatil19022006@gmail.com>
2025-10-21 11:03:08 +0530

    done with string literals

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 fn main() {
-    let mut num:u8 = 255;
-    println!("The value stored in num is {}",num);
-    num = 100;
-    println!("The value stored in num is {}",num)
+    // let mut num:u8 = 255;
+    // println!("The value stored in num is {}",num);
+    // num = 100;
+    // println!("The value stored in num is {}",num)
+
+    //String ---> Dynamic length Strings - Heap allocated
+    //&str --> Fixed length Strings - Special memory
+    let mut string_literal:String = String::from("Hii, Rajshri");
+    string_literal.push_str(" Wassup...");
+    print!("This is string literal {}", string_literal);
+
 }
 
