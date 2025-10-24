3c4e14aa4faf068d8b362988c2a51a9fc3952fb7
Rajshri Patil <rajshripatil19022006@gmail.com>
2025-10-24 16:54:53 +0530

    learnt tuples with destructuring

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,20 @@ fn main() {
 
     //String ---> Dynamic length Strings - Heap allocated
     //&str --> Fixed length Strings - Special memory
-    let mut string_literal:String = String::from("Hii, Rajshri");
-    string_literal.push_str(" Wassup...");
-    print!("This is string literal {}", string_literal);
+    // let mut string_literal:String = String::from("Hii, Rajshri");
+    // string_literal.push_str(" Wassup...");
+    // print!("This is string literal {}", string_literal);
 
+
+
+    //Tuple
+    let emp_info:(&str, u8) = ("Raj", 26);
+    let emp_name:&str = emp_info.0;
+    let emp_age:u8 = emp_info.1;
+    println!("Name : {} and Age : {}",emp_name, emp_age);
+    //destructuring
+    let (employee_n, employee_a) = emp_info;
+    println!("Employee Name : {} and Employee Age : {}", employee_n, employee_a);
+    
 }
 
